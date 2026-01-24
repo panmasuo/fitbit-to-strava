dd4b617439beff54f1ac8f8055c7848b5892f39d
pzar <pawezarem@gmail.com>
2026-01-24 09:28:43 +0100

    app: use strategy for Trim

--- src/main.cpp
+++ src/main.cpp
@@ -7,55 +7,35 @@
 #include "api.hpp"
 #include "json.hpp"
 
-/** 
- * @brief Custom viewable range adaptor for trimming provided string and keyword.
- *
- * 'trim_after' can be used as well with different subrange creation function.
- */
-struct trim_to : std::ranges::range_adaptor_closure<trim_to>
+template<typename Strategy>
+struct Trim : std::ranges::range_adaptor_closure<Trim<Strategy>>
 {
     std::string_view keyword;
+    Strategy subrange;
 
-    constexpr explicit trim_to(std::string_view keyword) : keyword(keyword) {}
+    constexpr explicit Trim(std::string_view keyword, Strategy callback) : keyword(keyword), subrange(callback) {}
 
-    /**
-     * @brief Look for provided keyword in the viewable range string. Call virtual
-     * 'create_subrange' method for returning desired subrange.
-     */
     auto operator()(std::ranges::viewable_range auto&& range) const -> std::string_view
     {
         const auto text = std::string_view{range};
         const auto match = std::string_view{std::ranges::search(text, this->keyword)};
+
         if (match.empty()) {
             return {};
         }
 
-        return create_subrange(match.begin(), text.begin(), text.end());
-    }
-
-    virtual auto create_subrange(std::string_view::const_iterator m_begin,
-                                 [[maybe_unused]] std::string_view::const_iterator r_begin,
-                                 std::string_view::const_iterator r_end
-    ) const -> std::string_view
-    {
-        return {m_begin, r_end};
+        return this->subrange(text, match);
     }
 };
 
-/**
- * @brief Same structure like 'trim_to' class, but different subrange creation.
- */
-struct trim_after : trim_to
+auto trim_to = [](std::string_view text, std::string_view match) -> std::string_view
 {
-    constexpr explicit trim_after(std::string_view keyword) : trim_to(keyword) {}
+    return {match.begin(), text.end()};
+};
 
-    auto create_subrange(std::string_view::const_iterator m_begin,
-                         std::string_view::const_iterator r_begin,
-                         [[maybe_unused]] std::string_view::const_iterator r_end
-    ) const -> std::string_view override
-    {
-        return {r_begin, m_begin};
-    }
+auto trim_after = [](std::string_view text, std::string_view match) -> std::string_view
+{
+    return {text.begin(), match.begin()};
 };
 
 auto main(int argc, char **argv) -> int
@@ -75,8 +55,8 @@ auto main(int argc, char **argv) -> int
     const auto from_keyword = std::string_view{"code="};
     const auto to_keyword = std::string_view{"#"};
 
-    const auto code = response_url | trim_to{from_keyword} | std::views::drop(from_keyword.size())
-              | trim_after{to_keyword} | std::ranges::to<std::string>();
+    const auto code = response_url | Trim{from_keyword, trim_to} | std::views::drop(from_keyword.size())
+              | Trim{to_keyword, trim_after} | std::ranges::to<std::string>();
 
     const auto& [auth_status_code, auth_text] = fitbit_post_authentication_request(
         client_id, client_secret, redirect_uri, code);
