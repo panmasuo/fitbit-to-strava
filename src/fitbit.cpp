476b7babd66622225f93d459e25ec5b488af499b
pzar <pawezarem@gmail.com>
2026-01-28 15:40:56 +0100

    fitbit: use session

--- src/fitbit.cpp
+++ src/fitbit.cpp
@@ -9,10 +9,16 @@ auto fitbit_create_authorization_url(
     const std::string& client_id, const std::string& redirect_uri
 ) -> std::string
 {
-    return std::format("https://www.fitbit.com/oauth2/authorize?response_type=code&"
-        "client_id={}&redirect_uri={}"
-        "&scope=activity%20heartrate", client_id, redirect_uri
-    );
+    auto session = cpr::Session{};
+
+    session.SetUrl({"https://www.fitbit.com/oauth2/authorize"});
+    session.SetParameters({
+        {"response_type", "code"},
+        {"client_id", client_id},
+        {"redirect_uri", redirect_uri}
+    });
+
+    return session.GetFullRequestUrl();
 }
 
 auto fitbit_post_authentication_request(
@@ -20,27 +26,39 @@ auto fitbit_post_authentication_request(
     const std::string &redirect_uri, const std::string &code
 ) -> std::tuple<int, std::string>
 {
-    const auto response = cpr::Post(
-        cpr::Url{"https://api.fitbit.com/oauth2/token"},
-        cpr::Payload{{"client_id", client_id}, {"grant_type", "authorization_code"},
-                     {"code", code}, {"redirect_uri", redirect_uri}},
-        cpr::Authentication{client_id, client_secret, cpr::AuthMode::BASIC}
-    );
+    auto session = cpr::Session();
+
+    session.SetUrl({"https://api.fitbit.com/oauth2/token"});
+    session.SetPayload({
+        {"client_id", client_id},
+        {"grant_type", "authorization_code"},
+        {"code", code},
+        {"redirect_uri", redirect_uri}
+    });
+    session.SetAuth({client_id, client_secret, cpr::AuthMode::BASIC});
+
+    const auto response = session.Post();
 
     return {response.status_code, response.text};
 }
 
 auto fitbit_get_activities(const std::string& access_token) -> std::tuple<int, std::string>
 {
-    const auto get_url = std::format(
-        "https://api.fitbit.com/1/user/-/activities/list.json?beforeDate={:%Y-%m-%d}&limit=5",
-        std::chrono::current_zone()->to_local(std::chrono::system_clock::now())
-    );
+    using namespace std::chrono;
 
-    const auto response = cpr::Get(
-        cpr::Url{get_url},
-        cpr::Header{{"Authorization", "Bearer " + access_token}}
-    );
+    auto session = cpr::Session{};
+    const auto today = std::format("{:%Y-%m-%d}", current_zone()->to_local(system_clock::now()));
+
+    session.SetUrl({"https://api.fitbit.com/1/user/-/activities/list.json"});
+    session.SetParameters({
+        {"beforeDate", today},
+        {"limit", "5"}
+    });
+    session.SetHeader({
+        {"Authorization", "Bearer " + access_token}
+    });
+
+    const auto response = session.Get();
 
     return {response.status_code, response.text};
 }
@@ -49,10 +67,14 @@ auto fitbit_get_heartrate(
     const std::string& heartrate_link, const std::string& access_token
 ) -> std::tuple<int, std::string>
 {
-    const auto response = cpr::Get(
-        cpr::Url{heartrate_link},
-        cpr::Header{{"Authorization", "Bearer " + access_token}}
-    );
+    auto session = cpr::Session{};
+
+    session.SetUrl({heartrate_link});
+    session.SetHeader({
+        {"Authorization", "Bearer " + access_token}
+    });
+
+    const auto response = session.Get();
 
     return {response.status_code, response.text};
 }
