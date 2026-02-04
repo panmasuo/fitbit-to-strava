1387694d616e830eaaefddf71d748f992533ffa3
pzar <pawezarem@gmail.com>
2026-02-04 22:38:55 +0100

    json: app: add comments

--- include/app.hpp
+++ include/app.hpp
@@ -6,6 +6,9 @@
 
 #include "nlohmann/json.hpp"
 
+/**
+ * Structure used for mapping workouts types from Fitbit API to Strava API.
+ */
 struct WorkoutName
 {
     [[nodiscard]] static auto convert_from(std::string_view fitbit_workout) -> std::string_view;
@@ -22,14 +25,22 @@ struct WorkoutName
     }};
 };
 
+/**
+ * Structure used for trimming strings.
+ */
 template<typename Strategy>
 struct Trim : std::ranges::range_adaptor_closure<Trim<Strategy>>
 {
+    /* To which, or from which keyword the trimming must be performed. */
     std::string_view keyword;
+
+    /* Callable function that will create subrange from given text and matched text. */
     Strategy subrange;
 
-    constexpr explicit Trim(std::string_view keyword, Strategy callback) : keyword(keyword), subrange(callback) {}
+    constexpr explicit Trim(std::string_view keyword, Strategy callback)
+        : keyword(keyword), subrange(callback) {}
 
+    /* Create two subranges, one unchanged text and second starting on given keyword. */
     auto operator()(std::ranges::viewable_range auto&& range) const -> std::string_view
     {
         const auto text = std::string_view{range};
@@ -43,17 +54,30 @@ struct Trim : std::ranges::range_adaptor_closure<Trim<Strategy>>
     }
 };
 
+/** 
+ * Trim out and return the insides of the string, from provided keyword (excluded) 
+ * to provided keyword (excluded).
+ */
 [[nodiscard]] auto trimmer(
     std::string_view text, std::string_view from_keyword, std::string_view to_keyword
 ) -> std::string;
 
+/**
+ * Prompt the user to access the URL and paste back returning URL.
+ */
 [[nodiscard]] auto get_code_from_url(
     std::string_view url, std::string_view from_keyword, std::string_view to_keyword
 ) -> std::string;
 
+/**
+ * Creates new TCX file using Fitbit activity with comibnation of provided heartrate time series.
+ */
 [[nodiscard]] auto create_tcx(
     const nlohmann::json& activity,
     const nlohmann::json& heartrate
 ) -> std::filesystem::path;
 
+/**
+ * Prompt the user about choosing the activity to post.
+ */
 [[nodiscard]] auto ask_for_workout(const std::vector<std::filesystem::path>& workouts) -> int;
--- include/json.hpp
+++ include/json.hpp
@@ -5,10 +5,19 @@
 
 #include "nlohmann/json.hpp"
 
+/**
+ * Loads provided file and parses it into JSON.
+ */
 [[nodiscard]] auto load(const std::filesystem::path& name) -> nlohmann::json;
 
+/**
+ * Parses string input into JSON.
+ */
 [[nodiscard]] auto parse(const std::string& content) -> nlohmann::json;
 
+/**
+ * Saves provided content into provided path.
+ */
 [[nodiscard]] auto save(
     const std::filesystem::path& name,
     const std::string& content
